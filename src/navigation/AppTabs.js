6b589a66d9578beb0ebee97c142d1bda1c317172
NicoleGG2008 <nicolegarciag1@gmail.com>
2026-05-19 23:06:29 -0600

    Cambios en AppTabs

--- src/navigation/AppTabs.js
+++ src/navigation/AppTabs.js
@@ -1,13 +1,49 @@
+import React from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "../screens/HomeScreen";
 
+import TransactionsScreen from "../screens/TransactionsScreen";
+import TransactionFormScreen from "../screens/TransactionFormScreen";
+
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function TransactionsStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen 
+        name="TransactionsList" 
+        component={TransactionsScreen} 
+      />
+      <Stack.Screen 
+        name="TransactionForm" 
+        component={TransactionFormScreen} 
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function AppTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Inicio" component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#2ecc71', 
+        tabBarInactiveTintColor: '#7f8c8d',
+      }}
+    >
+      <Tab.Screen 
+        name="Inicio" 
+        component={HomeScreen} 
+        options={{ title: 'Inicio' }}
+      />
+
+      <Tab.Screen 
+        name="Transacciones" 
+        component={TransactionsStack} 
+        options={{ title: 'Mis Finanzas' }}
+      />
     </Tab.Navigator>
   );
 }
